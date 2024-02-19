7b8af7e245bef6d7995d10605e5321399be1bc07
Dastin Sandura <d.kuwalek@gmail.com>
2024-02-19 13:31:43 +0100

    Add last case to the question

--- chapter 1 - building blocks/MainF.java
+++ chapter 1 - building blocks/MainF.java
@@ -1,5 +1,8 @@
 class MainF {
-	
+	//MainF.java:3: error: invalid method declaration; return type required
+        //public static main(String[] args) {
+      	//              ^
+	//1 error
 	public static main(String[] args) {
 		System.out.println("Main F. First argument: " + args[0] + " from total" + args.length);
 	}
