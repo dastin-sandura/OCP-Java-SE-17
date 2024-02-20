c83d1bef4bb52065cdf3d944fa40ad47e67a2a80
Dastin Sandura <d.kuwalek@gmail.com>
2024-02-20 12:12:50 +0100

    Chapter 1 exercise 2

---
+++ chapter 1 - building blocks/correct statement order/MainA.java
@@ -0,0 +1,15 @@
+class Rabbit {}
+import java.util.*;
+package animals;
+
+/*
+
+MainA.java:2: error: class, interface, enum, or record expected
+import java.util.*;
+^
+MainA.java:3: error: class, interface, enum, or record expected
+package animals;
+^
+2 errors
+
+*/
---
+++ chapter 1 - building blocks/correct statement order/MainB.java
@@ -0,0 +1,10 @@
+import java.util.*;
+package animals;
+class Rabbit {}
+/*
+MainB.java:2: error: class, interface, enum, or record expected
+package animals;
+^
+1 error
+
+*/
